698e165623b5fc8343eb27cd6c55d3d5769dc432
GavinWarnock <gbwarnock@gmail.com>
2023-04-06 12:52:42 -0600

    📦 NEW: Test Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import NavBar from './Components/NavigationBar/NavigationBar';
+import MusicTable from './Components/MusicTable/MusicTable';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <div>
+        <MusicTable />
+        </div>
     </div>
   );
 }
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,22 @@
+const MusicTable = (props) => {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Release Date</th>
+                <th>Genre</th>
+                <th>Running Time</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
+
+export default MusicTable;
